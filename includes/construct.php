d474d8efb3d83f29a72fc4961edf1c0fa9055d43
SearchengineTeam <66532163+SearchengineTeam@users.noreply.github.com>
2020-06-06 23:24:55 +0200

    Update construct.php

--- includes/construct.php
+++ includes/construct.php
@@ -46,3 +46,40 @@ class Adseo_Panel {
 		$this->define_public_hooks();
 
 	} 
+private function load_dependencies() {
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-adseo-panel-loader.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-adseo-panel-i18n.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-adseo-panel-admin.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-adseo-panel-public.php';
+		$this->loader = new Adseo_Panel_Loader();
+
+	}
+	private function set_locale() {
+		$plugin_i18n = new Adseo_Panel_i18n();
+		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+	}
+	private function define_metabox() {
+		$plugin_admin = new Adseo_Metabox_Admin( $this->get_plugin_name(), $this->get_version() );
+		//$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		//$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->
+	}
+	private function define_public_hooks() {
+		$plugin_public = new Adseo_Panel_Public( $this->get_plugin_name(), $this->get_version() );
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+	}
+	public function run() {
+		$this->loader->run();
+	}
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+	public function get_loader() {
+		return $this->loader;
+	}
+	public function get_version() {
+		return $this->version;
+	}
+
+}
