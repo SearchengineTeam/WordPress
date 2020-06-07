cbd31b9947306991e46c0a0be9d0e4332334df91
SearchengineTeam <66532163+SearchengineTeam@users.noreply.github.com>
2020-06-07 11:33:12 +0200

    Update construct.php

--- admin/construct.php
+++ admin/construct.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The admin-specific functionality of the plugin.
  *
@@ -20,50 +19,34 @@
  * @subpackage Adseo_Panel/admin
  * @author     Volkan Kücükbudak <plugins@searchengine.team>
  */
-class Adseo_Mewtabox_Admin {
+class Adseo_Metabox_Admin {
 	private $plugin_name;
 	private $version;
 	public function __construct( $plugin_name, $version ) {
-
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-
 	}
-
+	/* --------------------------------- load css to admin backend ------------------------- */
 	public function enqueue_styles() {
-
-
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/adseo-panel-admin.css', array(), $this->version, 'all' );
-
+	wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/adseo-panel-admin.css', array(), $this->version, 'all' );
 	}
-
-
+	/* --------------------------------- load js to admin backend ------------------------- */
 	public function enqueue_scripts() {
-
-
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/adseo-panel-admin.js', array( 'jquery' ), $this->version, false );
-
+	wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/adseo-panel-admin.js', array( 'jquery' ), $this->version, false );
 	}
-
-
-// Start Metabox
-
-// Create Metabox
-		public function wis_mb_create() {
-		$screens = array( 'post' , 'page' );
-		foreach ( $screens as $screen ) {
-		add_meta_box(
-            	'wis-bd',
-            	'Advanced SEO',
-            	'wis_list_function',
-            	$screen,
-            	'normal',
-            	'high'
-        	);
-    			}
-			}
+	/* --------------------------------- load js to admin backend ------------------------- */
+	public function adseo_metbox_create() {
+	$screens = array( 'post' , 'page' ); // to use customs type please put your arguments in it exp: 'portfolio' or 'customs_name' 
+	foreach ( $screens as $screen ) {
+	add_meta_box(
+        'ad-seo',
+        'Advanced SEO',
+        'ad_list_function',
+        $screen,
+        'normal',
+        'high'
+         ); }
+	}
 
 // class end
 
