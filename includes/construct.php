a87c5031dee469434f6da3dd94087369d4d88fb7
SearchengineTeam <66532163+SearchengineTeam@users.noreply.github.com>
2020-06-07 12:10:25 +0200

    Update construct.php

--- includes/construct.php
+++ includes/construct.php
@@ -59,15 +59,16 @@ private function load_dependencies() {
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 	}
 	private function define_metabox() {
-		$plugin_admin = new Adseo_Metabox_Admin( $this->get_plugin_name(), $this->get_version() );
-		//$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		//$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-		$this->loader->
+	require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/construct.php';
+	$plugin_admin = new Adseo_Metabox_Admin( $this->get_plugin_name(), $this->get_version() );
+		$this->loader->add_action( 'add_meta_boxes', $plugin_admin, 'adseo_metbox_create' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 	}
 	private function define_public_hooks() {
 		$plugin_public = new Adseo_Panel_Public( $this->get_plugin_name(), $this->get_version() );
-		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 	}
 	public function run() {
 		$this->loader->run();
